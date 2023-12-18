300cae72bdec2b3df7031fe7bb89763b6a67161d
raed <raed.da3k@gmail.com>
2023-12-18 13:21:42 +0100

    update state file

--- models/state.py
+++ models/state.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
-'''
-    Implementation of the State class
-'''
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+"""This is the state class"""
+from models.base_model import BaseModel
+from models.base_model import Base
 import models
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+
+"""
+# TO DO:
+# cities getter
+#for FileStorage: getter attribute cities that returns
+#the list of City instances with state_id equals to the
+#current State.id =>
+#It will be the FileStorage relationship between State and City
+"""
 
 
 class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-        Create relationship between class State (parent) to City (child)
-    '''
-    __tablename__ = "states"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """This is the class for State
+    Attributes:
+        name: input name
+    __tablename__: SQL table
+    cities: for DBStorage, creates relationship for class City to state table
+    """
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+        cities = relationship("City", backref="state")
     else:
-        name = ""
-
         @property
         def cities(self):
-            '''
-                Return list of city instances if City.state_id==current
-                State.id
-                FileStorage relationship between State and City
-            '''
-            list_cities = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+            """ Getter
+            """
+            r_v = []
+            objs = storage.all()
+            for key in objs.keys():
+                if key.split(".")[0] == "City":
+                    if key.split(".")[1] == self.id:
+                        r_v.append(objs[key])
+            return (r_v)
